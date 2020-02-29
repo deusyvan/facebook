5b019e358bf13363ff2b5e3f31e526f0fd96d7bc
Deusyvan DFS Web <contato@dfsweb.com.br>
2020-02-29 00:18:55 -0300

    Participando e sendo membro de um grupo

--- Controllers/GrupoController.php
+++ Controllers/GrupoController.php
@@ -24,8 +24,20 @@ class GrupoController extends Controller {
         );
         
         $dados['usuario_nome'] = $u->getNome($_SESSION['lgsist']);
+        $dados['info'] = $g->getInfo($id_grupo);
+        $dados['id_grupo'] = $id_grupo;
+        $dados['is_membro'] = $g->isMembro($id_grupo, $_SESSION['lgsist']);
+        $dados['qt_membros'] = $g->getQuantMembros($id_grupo);
         
         $this->loadTemplate('grupo', $dados);
+    }
+    
+    public function entrar($id_grupo)
+    {
+        $id_usuario = $_SESSION['lgsist'];
+        $g = new Grupos();
+        $g->addMembro($id_usuario, $id_grupo);
+        header("Location: ".BASE_URL."grupo/abrir/".$id_grupo);
     }
 
 }
--- Views/grupo.php
+++ Views/grupo.php
@@ -1,1 +1,7 @@
-Exemplo de grupo
+<h1><?php echo $info['titulo']; ?>(<?php echo $qt_membros; ?> membro<?php echo ($qt_membros == '1')? '':'s'?>)</h1>
+<?php if ($is_membro):?>
+Você é membro, parabéns
+<?php else: ?>
+	<h3>Você não é membro deste grupo.</h3>
+	<a href="<?php echo BASE_URL; ?>grupo/entrar/<?php echo $id_grupo; ?>" class="btn btn-default">Entrar no Grupo</a>
+<?php endif;?>
